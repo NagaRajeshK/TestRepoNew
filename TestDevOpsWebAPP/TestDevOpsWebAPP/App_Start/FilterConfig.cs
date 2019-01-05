7ea0cbb0687b3f4e484096c9817d6b6b8fafe239
Naga Rajesh Kotaprolu <NagaRajesh.Kotaprolu@Honeywell.com>
2019-01-05 10:18:38 +0530

    first commit

---
+++ TestDevOpsWebAPP/TestDevOpsWebAPP/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TestDevOpsWebAPP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
